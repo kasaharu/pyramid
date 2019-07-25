dec720fa38e43161000a08830a4efde0770aa111
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-25 09:15:25 +0900

    yarn ng g library utilities --prefix util --lint-fix

---
+++ projects/utilities/src/lib/utilities.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'util-utilities',
+  template: `
+    <p>
+      utilities works!
+    </p>
+  `,
+  styles: [],
+})
+export class UtilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
