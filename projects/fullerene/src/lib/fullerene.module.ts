8e1e68486f476341ad47d4fc4a693fa0658201af
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-28 13:43:15 +0900

    [fullerene] yarn ng g c components/organisms/header --project fullerene --lint-fix

--- projects/fullerene/src/lib/fullerene.module.ts
+++ projects/fullerene/src/lib/fullerene.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/organisms/header/header.component';
 import { FullereneComponent } from './fullerene.component';
 
 @NgModule({
-  declarations: [FullereneComponent],
+  declarations: [FullereneComponent, HeaderComponent],
   imports: [],
   exports: [FullereneComponent],
 })
