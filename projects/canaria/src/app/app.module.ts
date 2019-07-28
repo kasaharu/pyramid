82bf2aca8e112836bc03ef21860353bf2d3e2d57
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-28 15:14:17 +0900

    [canaria] yarn ng g c pages/login --project canaria --lint-fix

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
