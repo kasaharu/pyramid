cc6029367607b1407e3030d53afb7c9ba0fd0bc6
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-30 23:28:37 +0900

    [canaria] StoreModule の root 設定

--- projects/canaria/src/app/root-store/root-store.module.ts
+++ projects/canaria/src/app/root-store/root-store.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 import { TodoStoreModule } from './todo-store/todo-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, TodoStoreModule],
+  imports: [CommonModule, StoreModule.forRoot({}), TodoStoreModule],
 })
 export class RootStoreModule {}
