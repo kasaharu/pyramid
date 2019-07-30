c80625bbe2fa91ce8d908057801e3550935a59d8
Wataru.Kasahara <kasahara1019@gmail.com>
2019-07-30 23:04:16 +0900

    [canaria] yarn ng g module root-store/todo-store --flat false --module root-store/root-store.module.ts --project canaria

--- projects/canaria/src/app/root-store/root-store.module.ts
+++ projects/canaria/src/app/root-store/root-store.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoStoreModule } from './todo-store/todo-store.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, TodoStoreModule],
 })
 export class RootStoreModule {}
