b53d01c2252be05d6d9bde315e28d3e2845aeebb
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-02 20:27:01 +0900

    [canaria] TaskStore を featureStore として登録

--- projects/canaria/src/app/store/task-store/task-store.module.ts
+++ projects/canaria/src/app/store/task-store/task-store.module.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducer';
+import { featureName } from './selectors';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature(featureName, reducer)],
 })
 export class TaskStoreModule {}
