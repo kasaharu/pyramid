63d58f847c55bdcc794a0a1b7616d74d7df97a92
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-02 20:18:57 +0900

    [canaria] TaskStore の Selector を実装

---
+++ projects/canaria/src/app/store/task-store/selectors.spec.ts
@@ -0,0 +1,11 @@
+import { Task } from '../../domain/models';
+import { selectTaskList } from './selectors';
+import { State } from './state';
+
+describe('TaskStore selector spec', () => {
+  it('selectTaskList', () => {
+    const taskList: Task[] = [{ id: 1, title: '', isCompleted: false }];
+    const state: State = { taskList };
+    expect(selectTaskList.projector(state)).toEqual(taskList);
+  });
+});
