d90267a591dd75e4390efb7c7f8f09ab10b95681
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-04 16:23:05 +0900

    [fullerene] ButtonComponent の実装

--- projects/fullerene/src/lib/components/atoms/button/button.component.ts
+++ projects/fullerene/src/lib/components/atoms/button/button.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+
+export type ColorTypes = 'primary';
 
 @Component({
-  selector: 'fl-button',
+  // tslint:disable-next-line: component-selector
+  selector: 'button[flButton]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  @Input()
+  color: ColorTypes;
+  @HostBinding('class.primary')
+  get classIsPrimary() {
+    return this.color === 'primary';
+  }
+
   constructor() {}
 
   ngOnInit() {}
