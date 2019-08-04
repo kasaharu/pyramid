8f499c7d5f7f54f91852b560f50459387f90d82f
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-04 14:20:40 +0900

    [canaria] form 経由で作られた task を store へ登録するための usecase を実装

--- projects/canaria/src/app/usecases/task-list.usecase.spec.ts
+++ projects/canaria/src/app/usecases/task-list.usecase.spec.ts
@@ -1,12 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { skip } from 'rxjs/operators';
+import { FormedTask, Task } from '../domain/models';
+import { TaskStoreActions } from '../store/task-store';
 import { TaskListUsecase } from './task-list.usecase';
 
 describe('TaskListUsecase', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let usecase: TaskListUsecase;
+  let store$: MockStore<{}>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({ initialState: {} })],
+    });
+
+    usecase = TestBed.get(TaskListUsecase);
+    store$ = TestBed.get(Store);
+  });
 
   it('should be created', () => {
-    const usecase: TaskListUsecase = TestBed.get(TaskListUsecase);
     expect(usecase).toBeTruthy();
+  });
+
+  it('call addTask()', () => {
+    const formedTask: FormedTask = { title: 'test', isCompleted: false };
+    const task: Task = { ...formedTask, id: 1 };
+    const saveAction = TaskStoreActions.save(task);
+    const expected: Array<any> = [saveAction];
+
+    const actions: Array<any> = [];
+    store$.scannedActions$.pipe(skip(1)).subscribe((action) => actions.push(action));
+    usecase.addTask(formedTask);
+    expect(actions).toEqual(expected);
   });
 });
