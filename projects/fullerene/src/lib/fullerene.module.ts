bcbfe6af49e4046fc4b51b28969b98dc950281f9
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-04 15:40:10 +0900

    [fullerene] yarn ng g c components/atoms/button --project fullerene --lint-fix

--- projects/fullerene/src/lib/fullerene.module.ts
+++ projects/fullerene/src/lib/fullerene.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ButtonComponent } from './components/atoms/button/button.component';
 import { HeaderComponent } from './components/organisms/header/header.component';
 import { FullereneComponent } from './fullerene.component';
 
 @NgModule({
-  declarations: [FullereneComponent, HeaderComponent],
+  declarations: [FullereneComponent, HeaderComponent, ButtonComponent],
   imports: [],
   exports: [FullereneComponent, HeaderComponent],
 })
