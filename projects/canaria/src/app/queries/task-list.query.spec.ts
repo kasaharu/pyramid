2d33d9a125fdffea7815cc8296ff6bbbe04a9671
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-04 15:16:47 +0900

    [canaria] TaskStore から taskList を取得するための query を実装

--- projects/canaria/src/app/queries/task-list.query.spec.ts
+++ projects/canaria/src/app/queries/task-list.query.spec.ts
@@ -1,12 +1,44 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Task } from '../domain/models';
+import { TaskStoreSelectors } from '../store/task-store';
 import { TaskListQuery } from './task-list.query';
 
 describe('TaskListQuery', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let query: TaskListQuery;
+  let store$: MockStore<{}>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          initialState: {
+            [TaskStoreSelectors.featureName]: { taskList: [] },
+          },
+        }),
+      ],
+    });
+
+    query = TestBed.get(TaskListQuery);
+    store$ = TestBed.get(Store);
+  });
 
   it('should be created', () => {
-    const query: TaskListQuery = TestBed.get(TaskListQuery);
     expect(query).toBeTruthy();
+  });
+
+  describe('taskList$', () => {
+    it('default', () => {
+      query.taskList$.subscribe((value) => expect(value).toEqual([]));
+    });
+
+    it('updated', () => {
+      const taskList: Task[] = [{ id: 1, title: 'test', isCompleted: false }];
+      const newState = { [TaskStoreSelectors.featureName]: { taskList } };
+      store$.setState(newState);
+
+      query.taskList$.subscribe((value) => expect(value).toEqual(taskList));
+    });
   });
 });
