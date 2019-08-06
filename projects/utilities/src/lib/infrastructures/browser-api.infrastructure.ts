a165f17ab6a7486b51c2dac72d4c37195a8f762d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-06 10:07:23 +0900

    [util] Rename BrowserApiService -> BrowserApiInfrastructure

--- projects/utilities/src/lib/infrastructures/browser-api.infrastructure.ts
+++ projects/utilities/src/lib/infrastructures/browser-api.infrastructure.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class BrowserApiService {
-
-  constructor() { }
+export class BrowserApiInfrastructure {
+  constructor() {}
 }
