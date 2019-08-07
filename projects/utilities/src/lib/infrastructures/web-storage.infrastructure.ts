3eca6c8e894efc0d97bae806fb50111f37cd4eeb
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-08 00:58:23 +0900

    [util] WebStorageInfrastructure に get, set のメソッドを追加

--- projects/utilities/src/lib/infrastructures/web-storage.infrastructure.ts
+++ projects/utilities/src/lib/infrastructures/web-storage.infrastructure.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+// FIXME: localStorage のテストの書き方を調査して追加する
+/* istanbul ignore next */
 @Injectable({
   providedIn: 'root',
 })
 export class WebStorageInfrastructure {
   constructor() {}
+
+  get(keyName: string) {
+    return localStorage.getItem(keyName);
+  }
+
+  set(keyName: string, value: string) {
+    return localStorage.setItem(keyName, value);
+  }
 }
