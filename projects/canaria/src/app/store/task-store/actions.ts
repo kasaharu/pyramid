acdacb04d6ec17a45a2cf8c89d2d125ad29ffc5c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-10 15:44:37 +0900

    [canaria] タスク一覧を Store に保存するための action, reducer を実装

--- projects/canaria/src/app/store/task-store/actions.ts
+++ projects/canaria/src/app/store/task-store/actions.ts
@@ -1,6 +1,8 @@
 import { createAction, union } from '@ngrx/store';
 import { Task } from '../../domain/models';
 
+export const save = createAction('[Task] save', (payload: Task[]) => ({ payload }));
 export const create = createAction('[Task] create', (payload: Task) => ({ payload }));
-const actions = union({ create });
+
+const actions = union({ save, create });
 export type ActionsUnion = typeof actions;
--- projects/canaria/src/app/store/task-store/reducer.spec.ts
+++ projects/canaria/src/app/store/task-store/reducer.spec.ts
@@ -4,6 +4,14 @@ import { reducer } from './reducer';
 import { initialState, State } from './state';
 
 describe('TaskStore reducer spec', () => {
+  it('save action', () => {
+    const additionalTaskList: Task[] = [{ id: 1, title: 'test', isCompleted: false }];
+    const saveAction = Actions.save(additionalTaskList);
+    const expected: State = { taskList: additionalTaskList };
+
+    expect(reducer(initialState, saveAction)).toEqual(expected);
+  });
+
   it('create action', () => {
     const additionalTask: Task = { id: 1, title: 'test', isCompleted: false };
     const createAction = Actions.create(additionalTask);
