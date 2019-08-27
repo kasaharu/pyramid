6bd61ef69f89ec23fc6f61a10da1b3d518a632de
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-27 10:15:53 +0900

    [canaria] CurrentUserStore の actions, selectors, state を barrel export

---
+++ projects/canaria/src/app/store/current-user-store/index.ts
@@ -0,0 +1,5 @@
+import * as CurrentUserStoreActions from './actions';
+import * as CurrentUserStoreSelectors from './selectors';
+import * as CurrentUserStoreState from './state';
+
+export { CurrentUserStoreActions, CurrentUserStoreSelectors, CurrentUserStoreState };
