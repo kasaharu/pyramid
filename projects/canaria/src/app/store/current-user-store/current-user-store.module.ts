9f2db44258713aafe67a641b2d85d22527c0d139
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-27 10:18:49 +0900

    [canaria] CurrentUserStore を featureStore として登録

--- projects/canaria/src/app/store/current-user-store/current-user-store.module.ts
+++ projects/canaria/src/app/store/current-user-store/current-user-store.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducer';
+import { featureName } from './selectors';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, StoreModule.forFeature(featureName, reducer)],
 })
-export class CurrentUserStoreModule { }
+export class CurrentUserStoreModule {}
