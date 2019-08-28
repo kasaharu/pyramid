8d49a8d2b9bd226e0c88a068374cee517435cb84
Wataru.Kasahara <kasahara1019@gmail.com>
2019-08-28 22:50:19 +0900

    [canaria] Authenticator を使ったログインメソッドを LoginUsecase に実装

--- projects/canaria/src/app/usecases/login.usecase.spec.ts
+++ projects/canaria/src/app/usecases/login.usecase.spec.ts
@@ -1,12 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Authenticator } from 'utilities';
 import { LoginUsecase } from './login.usecase';
 
+class MockAuthenticator {
+  login() {}
+  logout() {}
+}
+
 describe('LoginUsecase', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let usecase: LoginUsecase;
+  let authenticator: Authenticator;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: Authenticator, useClass: MockAuthenticator }],
+    });
+
+    usecase = TestBed.get(LoginUsecase);
+    authenticator = TestBed.get(Authenticator);
+  });
 
   it('should be created', () => {
-    const usecase: LoginUsecase = TestBed.get(LoginUsecase);
     expect(usecase).toBeTruthy();
+  });
+
+  it('call login() method', () => {
+    spyOn(authenticator, 'login');
+
+    usecase.login();
+    expect(authenticator.login).toHaveBeenCalled();
+  });
+
+  it('call logout() method', () => {
+    spyOn(authenticator, 'logout');
+
+    usecase.logout();
+    expect(authenticator.logout).toHaveBeenCalled();
   });
 });
