2c32bc33764955e01abd966e48b0e2eab65e64ff
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-08 20:49:22 +0900

    [canaria] Firebase User の uid を Store に保存する処理を実装

--- projects/canaria/src/app/usecases/app-initialization.usecase.spec.ts
+++ projects/canaria/src/app/usecases/app-initialization.usecase.spec.ts
@@ -1,12 +1,51 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Store, Action } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { skip } from 'rxjs/operators';
+import { CurrentUserStoreActions } from '../store/current-user-store';
 import { AppInitializationUsecase } from './app-initialization.usecase';
 
 describe('AppInitializationUsecase', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AppInitializationUsecase;
+  let store$: MockStore<{}>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideMockStore({ initialState: {} })],
+    });
+
+    service = TestBed.get(AppInitializationUsecase);
+    store$ = TestBed.get(Store);
+  });
 
   it('should be created', () => {
-    const usecase: AppInitializationUsecase = TestBed.get(AppInitializationUsecase);
-    expect(usecase).toBeTruthy();
+    expect(service).toBeTruthy();
+  });
+
+  describe('call saveUserInfo()', () => {
+    it('loggedInUser が null の場合', async () => {
+      const user = null;
+      const expected: Action[] = [];
+      const actions: Action[] = [];
+
+      store$.scannedActions$.pipe(skip(1)).subscribe((action) => actions.push(action));
+
+      await service.saveUserInfo(user);
+      expect(actions).toEqual(expected);
+    });
+
+    it('call saveUserInfo()', async () => {
+      const uid = 'uid';
+      const user = {} as firebase.User;
+      user.uid = uid;
+      const saveCurrentUserAction = CurrentUserStoreActions.saveCurrentUser({ uid });
+      const expected = [saveCurrentUserAction];
+      const actions: Action[] = [];
+
+      store$.scannedActions$.pipe(skip(1)).subscribe((action) => actions.push(action));
+
+      await service.saveUserInfo(user);
+      expect(actions).toEqual(expected);
+    });
   });
 });
--- projects/canaria/src/app/usecases/app-initialization.usecase.ts
+++ projects/canaria/src/app/usecases/app-initialization.usecase.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CurrentUserStoreActions } from '../store/current-user-store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppInitializationUsecase {
-  constructor() {}
+  constructor(private store$: Store<{}>) {}
+
+  async saveUserInfo(loggedInUser: firebase.User | null) {
+    if (!loggedInUser) {
+      return;
+    }
+    this.store$.dispatch(CurrentUserStoreActions.saveCurrentUser({ uid: loggedInUser.uid }));
+  }
 }
