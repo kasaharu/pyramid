354c14148f1fcdca7ca70777f9c5b7235023e22d
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-08 21:13:45 +0900

    [canaria] アプリケーション起動時に Firebase User の uid を Store に保存

--- projects/canaria/src/app/app.component.spec.ts
+++ projects/canaria/src/app/app.component.spec.ts
@@ -1,20 +1,50 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BehaviorSubject } from 'rxjs';
+import { Authenticator } from 'utilities';
 import { AppComponent } from './app.component';
+import { AppInitializationUsecase } from './usecases/app-initialization.usecase';
+
+class MockAuthenticator {
+  loggedInUser$ = new BehaviorSubject<firebase.User | null>(null);
+}
+
+class MockAppInitializationUsecase {
+  saveUserInfo() {}
+}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let usecase: AppInitializationUsecase;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: Authenticator, useClass: MockAuthenticator },
+        { provide: AppInitializationUsecase, useClass: MockAppInitializationUsecase },
+      ],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    usecase = TestBed.get(AppInitializationUsecase);
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('call ngOnInit()', () => {
+    spyOn(usecase, 'saveUserInfo');
+    app.ngOnInit();
+
+    expect(usecase.saveUserInfo).toHaveBeenCalled();
   });
 });
