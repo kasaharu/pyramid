dab1598a9f5ed335286bedaa17c156aa5913dd33
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-12 09:45:20 +0900

    [canaria] TaskStore の actions を index に移動

--- projects/canaria/src/app/store/task-store/index.ts
+++ projects/canaria/src/app/store/task-store/index.ts
@@ -1,5 +1,12 @@
-import * as TaskStoreActions from './actions';
-import * as TaskStoreSelectors from './selectors';
-import * as TaskStoreState from './state';
+import { createAction, union } from '@ngrx/store';
+import { Task } from '../../domain/models';
 
-export { TaskStoreActions, TaskStoreSelectors, TaskStoreState };
+// NOTE: Actions
+export const saveTaskList = createAction('[Task] save', (payload: Task[]) => ({ payload }));
+export const createTask = createAction('[Task] create', (payload: Task) => ({ payload }));
+export const updateTask = createAction('[Task] update', (payload: Task) => ({ payload }));
+export const deleteTask = createAction('[Task] delete', (payload: string) => ({ payload }));
+
+export const Actions = { saveTaskList, createTask, updateTask, deleteTask };
+const ActionsUnion = union(Actions);
+type ActionsUnionType = typeof ActionsUnion;
