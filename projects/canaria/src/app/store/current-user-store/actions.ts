60ff441e2cec004ae9b0973a85cec909f71257fd
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-14 11:17:49 +0900

    [canaria] CurrentUserStore の不要になったファイルを削除

--- projects/canaria/src/app/store/current-user-store/actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, union } from '@ngrx/store';
-import { CurrentUser } from '../../domain/models';
-
-export const saveCurrentUser = createAction('[CurrentUser] save', (payload: CurrentUser) => ({ payload }));
-
-const actions = union({ saveCurrentUser });
-export type ActionsUnion = typeof actions;
