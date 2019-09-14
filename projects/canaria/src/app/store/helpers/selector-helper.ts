9a6b3ee1ec020b2b6011299e14f428e5fb0b5859
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-14 15:14:11 +0900

    [canaria] 高階関数の型を上の階層で受け取るように変更

--- projects/canaria/src/app/store/helpers/selector-helper.ts
+++ projects/canaria/src/app/store/helpers/selector-helper.ts
@@ -1,7 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-export function createFeatureStoreSelector(featureName: string) {
-  return <T, S>(mappingFunction: (state: T, props?: any) => S) => {
+export function createFeatureStoreSelector<T>(featureName: string) {
+  return <S>(mappingFunction: (state: T, props?: any) => S) => {
     return createSelector(
       createFeatureSelector<T>(featureName),
       mappingFunction,
