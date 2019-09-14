2c9e33a3017b32924491cafa86bcc1a082e60911
Wataru.Kasahara <kasahara1019@gmail.com>
2019-09-14 11:15:03 +0900

    [canaria] CurrentUserStore の spec ファイルを配置

---
+++ projects/canaria/src/app/store/current-user-store/index.spec.ts
@@ -0,0 +1,20 @@
+import { CurrentUser } from '../../domain/models';
+import { initialState, reducer, saveCurrentUser, selectCurrentUser, State } from './index';
+
+describe('CurrentUserStore reducer spec', () => {
+  it('save action', () => {
+    const currentUser: CurrentUser = { uid: '1' };
+    const saveAction = saveCurrentUser(currentUser);
+    const expected: State = { currentUser };
+
+    expect(reducer(initialState, saveAction)).toEqual(expected);
+  });
+});
+
+describe('CurrentUserStore selector spec', () => {
+  it('selectCurrentUser', () => {
+    const currentUser: CurrentUser = { uid: '1' };
+    const state: State = { currentUser };
+    expect(selectCurrentUser.projector(state)).toEqual(currentUser);
+  });
+});
