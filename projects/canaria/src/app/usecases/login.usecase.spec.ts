ccb9242cd66610af0015751e5606facdf456744c
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-05 10:11:33 +0900

    [canaria] login, logout 後に期待するページに遷移するように変更

--- projects/canaria/src/app/usecases/login.usecase.spec.ts
+++ projects/canaria/src/app/usecases/login.usecase.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { Authenticator } from 'utilities';
 import { LoginUsecase } from './login.usecase';
 
@@ -9,32 +11,39 @@ class MockAuthenticator {
 
 describe('LoginUsecase', () => {
   let usecase: LoginUsecase;
+  let router: Router;
   let authenticator: Authenticator;
 
   beforeEach(() => {
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
     TestBed.configureTestingModule({
-      providers: [{ provide: Authenticator, useClass: MockAuthenticator }],
+      imports: [RouterTestingModule],
+      providers: [{ provide: Router, useValue: routerSpy }, { provide: Authenticator, useClass: MockAuthenticator }],
     });
 
     usecase = TestBed.get(LoginUsecase);
     authenticator = TestBed.get(Authenticator);
+    router = TestBed.get(Router);
   });
 
   it('should be created', () => {
     expect(usecase).toBeTruthy();
   });
 
-  it('call login() method', () => {
+  it('call login() method', async () => {
     spyOn(authenticator, 'login');
 
-    usecase.login();
+    await usecase.login();
     expect(authenticator.login).toHaveBeenCalled();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/home');
   });
 
-  it('call logout() method', () => {
+  it('call logout() method', async () => {
     spyOn(authenticator, 'logout');
 
-    usecase.logout();
+    await usecase.logout();
     expect(authenticator.logout).toHaveBeenCalled();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/login');
   });
 });
