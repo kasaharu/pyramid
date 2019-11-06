4ef2a84415642980b50cd800d685c43337a58131
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-06 23:57:59 +0900

    [canaria] default import は AOT build だと実行時エラーになってしまうので修正

--- projects/canaria/src/app/store/task-store/task-store.module.ts
+++ projects/canaria/src/app/store/task-store/task-store.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-import reducer, { featureName } from './index';
+import { default as reducer, featureName } from './index';
 
 @NgModule({
   declarations: [],
