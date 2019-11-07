a7ed0fbcf0e88b6fdb97c42fb9d65900b3d5c996
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-07 10:04:43 +0900

    [canaria] タスクの削除ボタンをアイコンに変更

--- projects/canaria/src/app/app.module.ts
+++ projects/canaria/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -31,6 +32,7 @@ import { RootStoreModule } from './store/root-store.module';
     AngularFireAuthGuardModule,
     AngularFirestoreModule,
     MatCheckboxModule,
+    MatIconModule,
     MatListModule,
     UtilitiesModule,
     FullereneModule,
