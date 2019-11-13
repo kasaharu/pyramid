35852a5489be74e2142b4c4bc0f98762b811b3ba
Wataru.Kasahara <kasahara1019@gmail.com>
2019-11-13 10:13:41 +0900

    [canaria] yarn ng g c features/task/ui/section-list --module task.module --project canaria

--- projects/canaria/src/app/features/task/task.module.ts
+++ projects/canaria/src/app/features/task/task.module.ts
@@ -5,13 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { FullereneModule } from 'fullerene';
+import { SectionListComponent } from './ui/section-list/section-list.component';
 import { TaskFormComponent } from './ui/task-form/task-form.component';
 import { TaskItemComponent } from './ui/task-item/task-item.component';
 import { TaskListComponent } from './ui/task-list/task-list.component';
 import { TasksComponent } from './ui/tasks/tasks.component';
 
 @NgModule({
-  declarations: [TasksComponent, TaskListComponent, TaskItemComponent, TaskFormComponent],
+  declarations: [TasksComponent, TaskListComponent, TaskItemComponent, TaskFormComponent, SectionListComponent],
   imports: [CommonModule, ReactiveFormsModule, DragDropModule, MatCheckboxModule, MatIconModule, FullereneModule],
 })
 export class TaskModule {}
